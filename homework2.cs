18ac866f6e81c8aba2db2ccaef60900b982ae1bd
Almutasembelah201910509 <152531850+Almutasembelah201910509@users.noreply.github.com>
2023-11-30 21:54:10 +0300

    Add files via upload

---
+++ homework2.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{ 
+Class customer{
+public String name; 
+
+public Address; 
+}
+
+Class order {
+ 
+     private Date date;
+     private string status;
+ 
+
+
+}
+
+Class OrderDetail{
+
+private quality ;
+private sting taxstatus;
+
+
+}
+
+Class item{
+   Private shipping weight;
+   Private string description;
+
+
+
+}
+
+Class payment {
+
+Public float amount;
+
+
+}
+
+
+Class cash{
+
+   Public float cashTendered;
+
+
+}
+
+
+
+
+Class check {
+    
+    Public string name;
+    Public string bankID;
+
+}
+
+Class credit{
+  Public string name;
+  Public string type;
+  Public expDate;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
